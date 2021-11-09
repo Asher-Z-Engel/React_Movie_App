b475ea6e4ca9a86aa20e27792e932713c7ac85d1
Asher <asherzengel@gmail.com>
2021-11-09 19:58:07 +0200

    Route handling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Logo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
@@ -8,7 +9,9 @@ import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
 const Header = () => (
   <Wrapper>
     <Content>
-      <LogoImg src={Logo} alt='logo' />
+      <Link to='/'>
+        <LogoImg src={Logo} alt='logo' />
+      </Link>
       <TMDBLogoImg src={TMDBLogo} alt='TMDB Logo' />
 
     </Content>
